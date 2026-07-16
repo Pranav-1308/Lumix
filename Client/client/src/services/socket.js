5205036edf25d26047fadd7a04784f82bf31e97f
Pranav Gohil <lynx01676@gmail.com>
2026-07-16 14:17:33 +0530

    bypassed ngrok's interceptor

--- Client/client/src/services/socket.js
+++ Client/client/src/services/socket.js
@@ -4,6 +4,9 @@ const socket = io(import.meta.env.VITE_API_URL, {
   autoConnect: false,
   withCredentials: true,
   transports: ["websocket"],
+  extraheaders: {
+    "ngrok-skip-browser-warning": "any-value"
+  }
 });
 
 export default socket;
